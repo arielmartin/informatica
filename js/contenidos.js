b0352abfab16632205a3ea0c0d000edf9e0475b3
Ariel Martin <arielmartin80@gmail.com>
2021-06-06 22:01:28 -0300

    Precios modificables desde contenido.js

---
+++ js/contenidos.js
@@ -0,0 +1,7 @@
+/****** PRECIOS *******/
+
+var precio1 = "$ 1.500"
+var precio2 = "$ 500"
+var precio3 = "$ 1.000"
+
+export {precio1, precio2, precio3}
---
+++ js/imp.js
@@ -0,0 +1,7 @@
+import * as imp from "./contenidos.js";
+
+$(document).ready(function(){
+    $('#precio1').text(imp.precio1);
+    $('#precio2').text(imp.precio2);
+    $('#precio3').text(imp.precio3);
+});
