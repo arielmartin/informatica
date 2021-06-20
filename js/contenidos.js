58efb8e892d439d973d4b646e1d4cdd0c870ce2b
Ariel Martin <arielmartin80@gmail.com>
2021-06-20 17:46:20 -0300

    se agregó joystick y se ajustaron precios

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,7 +1,27 @@
-/****** PRECIOS *******/
+/****** PRECIOS  *******/
 
-var precio1 = "$ 1.500"
-var precio2 = "$ 500"
-var precio3 = "$ 1.000"
+//dIAGNOSTICO
+var precio1 = "SIN COSTO"
 
-export {precio1, precio2, precio3}
+//REINSTALACION DE SO
+var precio2 = "Costo $ 1.500"
+
+//costo BACK-UP
+var precio4 = "Costo $ 500"
+//MANTENIMIENTO DE EQUIPO
+
+var precio5 = "Costo $ 1.000"
+//costo instalacion antivirus
+
+var precio7 = "Costo $ 500"
+
+//costo LIMPIEZA DE SISTEMA
+var precio8 = "Costo $ 1.000"
+
+//costo mantenimiento de consolas
+var precio13 = "Costo $ 2.000"
+
+//costo mantenimiento de controles
+var precio14 = "Costo $ 1.000"
+
+export {precio1, precio2, precio4, precio5, precio7, precio8, precio13, precio14}
--- js/imp.js
+++ js/imp.js
@@ -1,7 +1,12 @@
 import * as imp from "./contenidos.js";
 
 $(document).ready(function(){
-    $('#precio1').text(imp.precio1);
-    $('#precio2').text(imp.precio2);
-    $('#precio3').text(imp.precio3);
+    $('.precio1').text(imp.precio1);
+    $('.precio2').text(imp.precio2);
+    $('.precio4').text(imp.precio4);
+    $('.precio5').text(imp.precio5);
+    $('.precio7').text(imp.precio7);
+    $('.precio8').text(imp.precio8);
+    $('.precio13').text(imp.precio13);
+    $('.precio14').text(imp.precio14);
 });
